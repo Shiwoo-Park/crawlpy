77b2b6823b573943895e72fe3e31e5e676ec7880
jsilva <woozin23@gmail.com>
2015-07-13 17:05:22 +0900

    update patterns

--- resource/html_patterns.py
+++ resource/html_patterns.py
@@ -1,1 +1,24 @@
-__author__ = 'jsilva'
+#!/usr/bin/env python
+# coding: utf-8
+
+PATTERN_DATA = """
+generator	xpressengine.com	meta name="Generator" content="XpressEngine"
+generator	xpressengine.com	div class="xe-widget-wrapper
+generator	blog.naver.com	type="application/wlwmanifest+xml" href="http://blog.naver.com
+generator	blog.naver.com	var blogURL = 'http://blog.naver.com';
+generator	tistory.com	var TOP_SSL_URL = 'https://www.tistory.com';
+dead	expired	서비스기간이 만료 되었습니다
+dead	expired	<div class="subtitle">Speak with a domain specialist!</div>
+dead	expired	<title>카페24 :: 대한민국 No.1 카페24 호스팅</title>
+generator	wordpress.org	<meta name="generator" content="WordPress
+generator	drupal.org	<meta name="Generator" content="Drupal
+generator	blog.daum.net	type="application/rss+xml" title="RSS" href="http://blog.daum.net
+generator	textcube.org	<meta name="generator" content="Textcube
+generator	blog.daum.net	<meta property="og:url" content="http://blog.daum.net
+generator	wordpress.com	<meta name="generator" content="WordPress.com"
+dead	expired	var refreshurl = "http://hostinfo.cafe24.com/serviceexpire/servicestop.html
+dead	expired	이 도메인은 요금 미납으로 도메인네임 서비스(DNS)가 정지되었습니다.
+dead	expired	<IMG src="img/logo.gif" border="0" target="dnsever">
+dead	expired	세도의 도메인 파킹</a>을 통해 무료로 제공되는 페이지입니다
+category	shopping	본 결제 창은 결제완료 후 자동으로 닫히며,결제 진행 중에 본 결제 창을 닫으시면
+"""
