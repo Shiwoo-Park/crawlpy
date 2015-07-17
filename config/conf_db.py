7511982f1b6a1d37f530154cb48a56729879d7d5
jsilva <woozin23@gmail.com>
2015-07-17 17:49:37 +0900

    added config files and req_url_crawler

---
+++ config/conf_db.py
@@ -0,0 +1,19 @@
+# coding:utf-8
+
+DB_INFO = dict()
+
+"""
+<INTRO>
+
+각 DB 서버에 대한 DB 계정정보를 정의하는 모듈
+
+- Dictionary의 KEY는 자신이 편한대로 지정한다.
+
+db_info_key = "local"  # DB 를 대표할 key
+DB_INFO[db_info_key] = dict()
+DB_INFO[db_info_key]["host"] = "localhost"  # IP 혹은 도메인 입력
+DB_INFO[db_info_key]["user"] = "root"  # DB 사용자명
+DB_INFO[db_info_key]["pwd"] = "1234"   # DB 암호
+DB_INFO[db_info_key]["db"] = "testdb"  # 접속할 DB 명
+"""
+
---
+++ db_util.py
@@ -0,0 +1,153 @@
+#!/usr/bin/env python2.7
+# coding: utf8
+
+import MySQLdb
+import MySQLdb.cursors
+from glance.config.db_info import DB_INFO
+from glance.utils.log import getLogger
+
+
+def getDBConnection(host, usr, pwd, db, cursor_type="normal"):
+	db_connect = None
+	db_cursor = None
+	try:
+		if cursor_type == "dict":
+			db_connect = MySQLdb.connect(host, usr, pwd, db, cursorclass=MySQLdb.cursors.DictCursor)
+		else:
+			db_connect = MySQLdb.connect(host, usr, pwd, db)
+		db_connect.set_character_set('utf8')
+		db_cursor = db_connect.cursor()
+	except Exception, msg:
+		getLogger().error("getDBCursor() Failed : %s"%msg)
+	return db_connect, db_cursor
+
+
+def getDBConnectionByName(name, cursor_type="normal"):
+	if name in DB_INFO:
+		return getDBConnection(DB_INFO["host"], DB_INFO["user"], DB_INFO["pwd"], DB_INFO["db"], cursor_type)
+	return None
+
+
+def selectQuery(dbCursor, query, params=()):
+	dbCursor.execute(query, params)
+	result = dbCursor.fetchall()
+	return result
+
+
+def executeQuery(dbCursor, query, params=(), desc=""):
+	ret = desc
+	dbCursor.execute(query, params)
+	dbCursor.execute("commit")
+	ret += " Success"
+	return ret
+
+
+# 쿼리의 WHERE 부분
+def makeWhereClause(dic, keyList=None):
+	whereClause = ""
+	params = []
+	if dic:
+		for key,val in dic.items():
+			add = False
+			if keyList:
+				if key in keyList:
+					add = True
+			else:
+				add = True
+
+			if add:
+				if len(params) > 0:
+					whereClause += " AND "
+				whereClause += key+"=%s"
+				params.append(val)
+
+	return (whereClause, params)
+
+
+def selectCount(dbCursor, tableName, whereDic=None, where="", params=()):
+	if whereDic :
+		where, params = makeWhereClause(whereDic)
+		if len(params) > 0:
+			where = " WHERE "+where
+
+	query = "SELECT COUNT(1) FROM "+tableName + where
+	dbCursor.execute(query, params)
+	ret = dbCursor.fetchone()
+	return ret[0]
+
+def selectData(dbCursor, selectList, tableName, whereDic=None, where="", params=(), order="", limit=""):
+	colStr = ""
+
+	for col in selectList:
+		colStr += col+","
+	if len(colStr) > 0:
+		colStr = colStr[:-1]
+
+	if whereDic:
+		where, params = makeWhereClause(whereDic)
+		if len(params) > 0:
+			where = " WHERE "+where
+
+	query = "SELECT "+colStr+" FROM "+tableName + where + order + limit
+
+	dbCursor.execute(query, params)
+	ret = dbCursor.fetchall()
+	return ret
+
+def insertData(dbCursor, desc, tableName, dataDic, isIgnore=False):
+	ret = "Insert "+desc+" Data"
+	fields = ""
+	values = ""
+	params = []
+
+	for key, val in dataDic.items() :
+		fields += key+","
+		values += "%s,"
+		params.append(val)
+	if len(params) > 0:
+		fields = fields[:-1]
+		values = values[:-1]
+
+	if isIgnore:
+		query = "INSERT IGNORE INTO "+tableName+" ("+fields+") VALUES ("+values+")"
+	else:
+		query = "INSERT INTO "+tableName+" ("+fields+") VALUES ("+values+")"
+
+	dbCursor.execute(query, params)
+	dbCursor.execute("commit")
+	ret += " : Success"
+	return ret
+
+def updateData(dbCursor, desc, tableName, setDic, whereDic=None):
+	ret = "Update "+desc+" Data"
+	setClause = ""
+	params = []
+	for key,val in setDic.items():
+		if len(params) > 0:
+			setClause += ", "
+		setClause += key+"=%s"
+		params.append(val)
+
+	whereClause = ""
+	if whereDic:
+		whereClause, whereParams = makeWhereClause(whereDic)
+		whereClause = " WHERE "+whereClause
+		params = params + whereParams
+
+	query = "UPDATE "+tableName+" SET "+setClause + whereClause
+	dbCursor.execute(query, params)
+	dbCursor.execute("commit")
+	ret += " : Success"
+	return ret
+
+def deleteData(dbCursor, desc, tableName, whereDic):
+	ret = "Delete "+desc+" Data"
+	whereClause, params = makeWhereClause(whereDic)
+	query = "DELETE FROM "+tableName+" WHERE "+whereClause
+	dbCursor.execute(query, params)
+	dbCursor.execute("commit")
+	ret += " : Success"
+	return ret
+
+if __name__ == "__main__":
+	pass
