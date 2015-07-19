7acccd1686ff2cf201da96b36acc6737bec34dec
silva23 <woozin23@gmail.com>
2015-07-19 16:23:46 +0900

    update url parsers

---
+++ psw_url_parser.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python2.7
+# -*- coding:utf-8 -*-
+
+"""
+URL 패턴 지정 Rule에 의해
+URL 인식과  Normalizing을 지원하고,
+특정 부분을 파싱하여 원하는 K,V형태로 제공하는 모듈
+
+1. 기능
+- 숫자 문자열 구분
+- HOST KEY 별 패턴매칭
+- 날짜값 파싱
+- URL 타입
+"""
+
+from model.common import URLData
+from url_parser import URLParser
+from str_util import isNumStr
+
+class PswURLFactory:
+
+if __name__ == '__main__':
+
+	pass
